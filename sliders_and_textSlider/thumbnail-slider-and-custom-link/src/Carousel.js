a6b29deef91cfbf12395a723a4e15c83706e82e9
Nomaan-i2a <noman.na@akounto.com>
2026-03-06 13:22:00 +0530

    new

--- sliders_and_textSlider/thumbnail-slider-and-custom-link/src/Carousel.js
+++ sliders_and_textSlider/thumbnail-slider-and-custom-link/src/Carousel.js
@@ -3,26 +3,66 @@ import { Carousel } from "react-responsive-carousel";
 import Image from "./logo.png";
 
 export default () => (
-  <Carousel autoPlay infiniteLoop interval="5000" transitionTime="1000" showArrows={true}>
-    <div>
-      <img alt="" src={Image} />
-      <p className="legend">Legend 1</p>
+  <>
+    <style>
+      {`
+        .sliderArea{
+          max-width:600px;
+          margin:auto;
+        }
+
+        .sliderArea img{
+          width:100%;
+          object-fit:cover;
+        }
+
+        .legend{
+          background:rgba(0,0,0,0.6);
+          color:#fff;
+          font-size:18px;
+          padding:10px;
+        }
+      `}
+    </style>
+    <div className="sliderArea">
+      <Carousel autoPlay infiniteLoop interval="5000" transitionTime="1000" showArrows={true}>
+        <div>
+          <img alt="" src={Image} />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 4</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 5</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 5</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 6</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 7</p>
+        </div>
+        <div>
+        <img alt="" src={Image} />
+          <p className="legend">Legend 8</p>
+        </div>
+      </Carousel>
     </div>
-    <div>
-    <img alt="" src={Image} />
-      <p className="legend">Legend 2</p>
-    </div>
-    <div>
-    <img alt="" src={Image} />
-      <p className="legend">Legend 3</p>
-    </div>
-    <div>
-    <img alt="" src={Image} />
-      <p className="legend">Legend 4</p>
-    </div>
-    <div>
-    <img alt="" src={Image} />
-      <p className="legend">Legend 5</p>
-    </div>
-  </Carousel>
+  </>
 );
