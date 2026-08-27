e8ac97ae5f35230590d542b8f1a87ffa39995901
nomaanarif786 <your-email@example.com>
2026-08-27 17:45:33 +0530

    new

--- tutorials/MERN-Project/backend-back/server.js
+++ tutorials/MERN-Project/backend-back/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -10,8 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("API is running.");
--- tutorials/MERN-Project/backend/config/db.js
+++ tutorials/MERN-Project/backend/config/db.js
@@ -1,12 +1,31 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(process.env.MONGO_URL);
+//         console.log("MongoDB connected successfully.");
+//     } catch (error) {
+//         console.log(`Error: ${error.message}`);
+//     }
+// }
+
+// export default connectDB;
+
+
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("MongoDB connected successfully.");
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-    }
-}
+  try {
+    const mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
 
-export default connectDB;
+    await mongoose.connect(uri);
+    console.log(`MongoDB Connected (Memory Server): ${uri}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB; // <--- Yeh hona zaroori hai!
