40a5af95f4d68bb1b5d8b9642059731180bfd72b
nomaanarif786 <your-email@example.com>
2026-08-27 18:50:41 +0530

    new

--- tutorials/NodeJs/Thapa/app.js
+++ tutorials/NodeJs/Thapa/app.js
@@ -1,9 +1,18 @@
-// const add = require("./math.js");
+// import {add, sub, mul, div, PI} from "./math.js";
 
-import {add, sub, mul, div, PI} from "./math.js";
+// console.log(add(4, 2));
+// console.log(sub(4, 2));
+// console.log(mul(4, 2));
+// console.log(div(4, 2));
+// console.log(PI * 7);
 
-console.log(add(4, 2));
-console.log(sub(4, 2));
-console.log(mul(4, 2));
-console.log(div(4, 2));
-console.log(PI * 7);
+
+
+
+const math = require("./math");
+
+console.log(math.add(4, 2));
+console.log(math.sub(4, 2));
+console.log(math.mul(4, 2));
+console.log(math.div(4, 2));
+console.log(math.PI * 7);
