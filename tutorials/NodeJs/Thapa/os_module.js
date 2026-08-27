b990c15ee3cfad7a7f56110df9d4b4893c9c0eb2
nomaanarif786 <your-email@example.com>
2026-08-27 19:18:43 +0530

    new

---
+++ tutorials/NodeJs/Thapa/os_module.js
@@ -0,0 +1,8 @@
+// https://nodejs.org/docs/latest/api/path.html
+
+const os = require("os");
+console.log("Platform = ", os.platform());
+console.log("User = ", os.userInfo());
+console.log("CPU Architecture = ", os.arch());
+console.log("Free Memory = ", os.freemem(), "bytes");
+console.log("Total Memory = ", os.totalmem(), "bytes");
