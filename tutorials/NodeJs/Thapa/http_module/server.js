627315205e1f3e23e0096c19a92d37e5c898e269
nomaanarif786 <your-email@example.com>
2026-08-28 18:16:55 +0530

    new

--- tutorials/NodeJs/Thapa/http_module/server.js
+++ tutorials/NodeJs/Thapa/http_module/server.js
@@ -1,1 +1,19 @@
-sD
+const http = require("http");
+const server = http.createServer((req, res) => {
+    if(req.url === "/"){
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h1>Home Page</h1>");
+        res.end();
+    }
+    if(req.url === "/about"){
+        res.setHeader("Content-Type", "text/plain");
+        res.write("About Page");
+        res.end();
+    }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`localhost:/${PORT}`);
+});
