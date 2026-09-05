7b65f5ffccf3c8fc32219ba3332f4d35ff62df9d
Enrique-UP <95688780+Enrique-UP@users.noreply.github.com>
2026-09-06 00:07:53 +0530

    new

---
+++ tutorials/NodeJs/Thapa/projects/urlShortner/part2/app.js
@@ -0,0 +1,31 @@
+import { readFile } from 'fs/promises';
+import { createServer } from 'http';
+import path from 'path';
+
+const PORT = 3002;
+
+const serveFile = async (res, filePath, contentType) => {
+    try {
+        const data = await readFile(filePath);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+    } catch (error) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 page not found');
+    }
+}
+
+const server = createServer(async (req, res) => {
+    console.log(req.url);
+    if (req.method === 'GET') {
+        if (req.url === '/') {
+            return serveFile(res, path.join('./', 'index.html'), 'text/html');
+        } else if (req.url === '/style.css') {
+            return serveFile(res, path.join('./', 'style.css'), 'text/css');
+        }
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
